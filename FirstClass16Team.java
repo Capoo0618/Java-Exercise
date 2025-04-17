9633625c13b6b3dd7a23f090f96c45a15af011a5
thugcreeper <nhpss100032@gmail.com>
2025-04-17 21:12:15 +0800

    01257007_commit
    加上學號姓名

--- FirstClass16Team.java
+++ FirstClass16Team.java
@@ -3,6 +3,7 @@ public class FirstClass16Team {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號:\"01257002\"\n姓名:\"張宸翊\"");
         System.out.println("\"學號:\"01257053\"\n姓名:\"傅姵瑄\"");
+        System.out.println("\"學號:\"01257007\"\n姓名:\"王洪賢\"");
     }
 }
     
